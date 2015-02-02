9fcf2adca6584e008872855e5a70956dc018b390
Aiden Keating <akeating@tssg.org>
2015-02-02 12:41:51 +0000

    Logs map to host

--- utils/run_all.sh
+++ utils/run_all.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
-#                 Container                                              Image
-docker run -d --name openidao            -p 80:80 -p 443:443        openiicteu/dao
-docker run -d --name openicomms          --net=container:openidao   openiicteu/communications
-docker run -d --name openinotifications  --net=container:openidao   openiicteu/notifications
-docker run -d --name openiauthapi        --net=container:openidao   openiicteu/authapi
-docker run -d --name openiattachmentapi  --net=container:openidao   openiicteu/attachmentapi
-docker run -d --name openiobjectapi      --net=container:openidao   openiicteu/objectapi
-docker run -d --name openipermissionsapi --net=container:openidao   openiicteu/permissionsapi
-docker run -d --name openiswagger        --net=container:openidao   openiicteu/swaggerdef
-docker run -d --name openicloudletapi    --net=container:openidao   openiicteu/cloudletapi
-docker run -d --name openisearchapi      --net=container:openidao   openiicteu/searchapi
-docker run -d --name openitypeapi        --net=container:openidao   openiicteu/typeapi
-docker run -d --name openimongrel2       --net=container:openidao   openiicteu/mongrel2
+HOST_LOG_PATH="/"
+CONT_LOG_PATH="/opt/openi/cloudlet_platform/logs/"
+
+#                 Container                                                                               Image
+docker run -d --name openidao            -v $HOST_LOG_PATH:$CONT_LOG_PATH -p 80:80 -p 443:443        openiicteu/dao
+docker run -d --name openicomms          -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/communications
+docker run -d --name openinotifications  -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/notifications
+docker run -d --name openiauthapi        -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/authapi
+docker run -d --name openiattachmentapi  -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/attachmentapi
+docker run -d --name openiobjectapi      -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/objectapi
+docker run -d --name openipermissionsapi -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/permissionsapi
+docker run -d --name openiswagger        -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/swaggerdef
+docker run -d --name openicloudletapi    -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/cloudletapi
+docker run -d --name openisearchapi      -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/searchapi
+docker run -d --name openitypeapi        -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/typeapi
+docker run -d --name openimongrel2       -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/mongrel2
 
 
 
