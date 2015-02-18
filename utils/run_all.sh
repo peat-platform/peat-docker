283c801a44fb7348e657eec4a7fc9c69269c31dc
Aiden Keating <akeating@tssg.org>
2015-02-18 14:52:30 +0000

    Fixed cbase disconnecting at run time

--- utils/run_all.sh
+++ utils/run_all.sh
@@ -8,18 +8,18 @@ CB_VOL_PATH="/home/openi/couchbase"
 #                 Container                                                                               Image
 docker run -d --name openicb -v $CB_VOL_PATH:/opt/couchbase/var -p 443:443 -p 80:80 -p 8091:8091  openiicteu/couchbase
 sleep 60
-docker run -d --name openidao            -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao  openiicteu/dao
+docker run -d --name openidao            -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb  openiicteu/dao
 #docker run -d --name openicomms          -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/communications
-docker run -d --name openinotifications  -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/notifications
-docker run -d --name openiauthapi        -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/authapi
-docker run -d --name openiattachmentapi  -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/attachmentapi
-docker run -d --name openiobjectapi      -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/objectapi
-docker run -d --name openipermissionsapi -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/permissionsapi
-docker run -d --name openiswagger        -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/swaggerdef
-docker run -d --name openicloudletapi    -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/cloudletapi
-docker run -d --name openisearchapi      -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/searchapi
-docker run -d --name openitypeapi        -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/typeapi
-docker run -d --name openimongrel2       -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openidao   openiicteu/mongrel2
+docker run -d --name openinotifications  -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/notifications
+docker run -d --name openiauthapi        -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/authapi
+docker run -d --name openiattachmentapi  -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/attachmentapi
+docker run -d --name openiobjectapi      -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/objectapi
+docker run -d --name openipermissionsapi -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/permissionsapi
+docker run -d --name openiswagger        -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/swaggerdef
+docker run -d --name openicloudletapi    -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/cloudletapi
+docker run -d --name openisearchapi      -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/searchapi
+docker run -d --name openitypeapi        -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/typeapi
+docker run -d --name openimongrel2       -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/mongrel2
 
 
 
