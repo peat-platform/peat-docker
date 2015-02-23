152eaf01a644270384dd690285740ba9f8e20bb9
Aiden Keating <akeating@tssg.org>
2015-02-23 16:17:49 +0000

    Added log path config file

--- utils/run_all.sh
+++ utils/run_all.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
 
-HOST_LOG_PATH="/"
-CONT_LOG_PATH="/opt/openi/cloudlet_platform/logs/"
+C_LOG_PATH="/opt/openi/cloudlet_platform/logs/"
 
-CB_VOL_PATH="/home/openi/couchbase"
+CB_LOG_PATH="/opt/couchbase/var/lib/couchbase/logs/"
+
+M_LOG_PATH="/mongrel2/logs/"
+
+source conf
 
 #                 Container                                                                               Image
-docker run -d --name openicb -v $CB_VOL_PATH:/opt/couchbase/var -p 443:443 -p 80:80 -p 8091:8091  openiicteu/couchbase
+docker run -d --name openicb -v $LOG_PATH:$CB_LOG_PATH -p 443:443 -p 80:80 -p 8091:8091  openiicteu/couchbase
 sleep 60
-docker run -d --name openidao            -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/dao
-docker run -d --name openinotifications  -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/notifications
-docker run -d --name openiauthapi        -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/authapi
-docker run -d --name openiattachmentapi  -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/attachmentapi
-docker run -d --name openiobjectapi      -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/objectapi
-docker run -d --name openipermissionsapi -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/permissionsapi
-docker run -d --name openiswagger        -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/swaggerdef
-docker run -d --name openicloudletapi    -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/cloudletapi
-docker run -d --name openisearchapi      -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/searchapi
-docker run -d --name openitypeapi        -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/typeapi
-docker run -d --name openihttpswagger    -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/httpswaggerdef
+docker run -d --name openidao            -v $LOG_PATH:$C_LOG_PATH --net=container:openicb   openiicteu/dao
+docker run -d --name openinotifications  -v $LOG_PATH:$C_LOG_PATH --net=container:openicb   openiicteu/notifications
+docker run -d --name openiauthapi        -v $LOG_PATH:$C_LOG_PATH --net=container:openicb   openiicteu/authapi
+docker run -d --name openiattachmentapi  -v $LOG_PATH:$C_LOG_PATH --net=container:openicb   openiicteu/attachmentapi
+docker run -d --name openiobjectapi      -v $LOG_PATH:$C_LOG_PATH --net=container:openicb   openiicteu/objectapi
+docker run -d --name openipermissionsapi -v $LOG_PATH:$C_LOG_PATH --net=container:openicb   openiicteu/permissionsapi
+docker run -d --name openiswagger        -v $LOG_PATH:$C_LOG_PATH --net=container:openicb   openiicteu/swaggerdef
+docker run -d --name openicloudletapi    -v $LOG_PATH:$C_LOG_PATH --net=container:openicb   openiicteu/cloudletapi
+docker run -d --name openisearchapi      -v $LOG_PATH:$C_LOG_PATH --net=container:openicb   openiicteu/searchapi
+docker run -d --name openitypeapi        -v $LOG_PATH:$C_LOG_PATH --net=container:openicb   openiicteu/typeapi
+docker run -d --name openihttpswagger    -v $LOG_PATH:$C_LOG_PATH --net=container:openicb   openiicteu/httpswaggerdef
 docker run -d --name openiredirect                                        --net=container:openicb   openiicteu/redirect
-docker run -d --name openimongrel2       -v $HOST_LOG_PATH:$CONT_LOG_PATH --net=container:openicb   openiicteu/mongrel2
+docker run -d --name openimongrel2       -v $LOG_PATH:$M_LOG_PATH --net=container:openicb   openiicteu/mongrel2
 
 
 
