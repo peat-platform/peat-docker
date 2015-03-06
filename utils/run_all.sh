bb46ddfd811919231775e3bb1300f9a9104ed9bc
Aiden Keating <akeating@tssg.org>
2015-03-06 12:26:59 +0000

    Fixed redirect update bug

--- utils/run_all.sh
+++ utils/run_all.sh
@@ -6,7 +6,7 @@ source conf
 docker run -d --name openicb -v $CB_OUTPUT_PATH:$CB_PATH -p 9200:9200  -p 443:443 -p 80:80 -p 8091:8091  openiicteu/couchbase
 sleep 60
 docker run -d --name openidao            -v $OUTPUT_PATH:$C_LOG_PATH --net=container:openicb   openiicteu/dao
-docker run -d --name openies                                      --net=container:openicb   openiicteu/elasticsearch
+docker run -d --name openies                                         --net=container:openicb   openiicteu/elasticsearch
 docker run -d --name openinotifications  -v $OUTPUT_PATH:$C_LOG_PATH --net=container:openicb   openiicteu/notifications
 docker run -d --name openiauthapi        -v $OUTPUT_PATH:$C_LOG_PATH --net=container:openicb   openiicteu/authapi
 docker run -d --name openiattachmentapi  -v $OUTPUT_PATH:$C_LOG_PATH --net=container:openicb   openiicteu/attachmentapi
@@ -17,9 +17,9 @@ docker run -d --name openicloudletapi    -v $OUTPUT_PATH:$C_LOG_PATH --net=conta
 docker run -d --name openisearchapi      -v $OUTPUT_PATH:$C_LOG_PATH --net=container:openicb   openiicteu/searchapi
 docker run -d --name openitypeapi        -v $OUTPUT_PATH:$C_LOG_PATH --net=container:openicb   openiicteu/typeapi
 docker run -d --name openihttpswagger    -v $OUTPUT_PATH:$C_LOG_PATH --net=container:openicb   openiicteu/httpswaggerdef
-docker run -d --name openiredirect                                --net=container:openicb   openiicteu/redirect
-docker run -d --name openiadmindash                               --net=container:openicb   openiicteu/admin-dashboard
-docker run -d --name openiauthdialogs                             --net=container:openicb   openiicteu/auth-dialogs
+docker run -d --name openiredirect                                   --net=container:openicb   openiicteu/redirect
+docker run -d --name openiadmindash                                  --net=container:openicb   openiicteu/admin-dashboard
+docker run -d --name openiauthdialogs                                --net=container:openicb   openiicteu/auth-dialogs
 docker run -d --name openimongrel2       -v $OUTPUT_PATH:$M_LOG_PATH --net=container:openicb   openiicteu/mongrel2
 
 
