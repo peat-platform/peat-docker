0eed3c4bc4a18a6307154b8833bc6d3f49e022e5
dmccarthy <dmccarthy@tssg.org>
2015-05-28 14:32:56 +0100

    Fixed run scripts; JSOn parameters now work

--- auth-dialogs/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-redis-server &
-node /openi-auth-dialogs/bin/www -s oMF81IOFsZ0bvzSdcBV -p "'-----BEGIN PUBLIC KEY-----\nMFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKT8kGk6ZNo3sC4IIo29leRLVD23T2r0\nvWXBEkk2pV42HsxKAmPs789AGHH9XwbGpD7FvrcBWWgb65v32Hg/NGkCAwEAAQ==\n-----END PUBLIC KEY-----'"
